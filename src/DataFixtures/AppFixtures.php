194f541c3b86fa44c4633cfefc7f1623b7b55206
jloppad <jloppad@g.educannd.es>
2024-05-17 11:15:08 +0200

    Datos de prueba

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,10 +3,15 @@
 namespace App\DataFixtures;
 
 use App\Entity\CursoAcademico;
+use App\Entity\Motivo;
 use App\Factory\CursoAcademicoFactory;
 use App\Factory\EstudianteFactory;
 use App\Factory\GrupoFactory;
+use App\Factory\LlaveFactory;
+use App\Factory\MotivoFactory;
+use App\Factory\RegistroFactory;
 use App\Factory\UsuarioFactory;
+use App\Repository\UsuarioRepository;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -16,6 +21,9 @@ class AppFixtures extends Fixture
     {
 
         $cursosAcademicos = [];
+        $motivos = ['Baño','Conserjeria','Despacho','Otro'];
+        date_default_timezone_set('Europe/Madrid');
+        $hora = date('H:i:s');
 
         for ($i = 0; $i < 2; $i++) {
             $cursosAcademicos[] = CursoAcademicoFactory::createOne();
@@ -26,13 +34,11 @@ class AppFixtures extends Fixture
             'password' => 'norris',
             'admin' => true
         ]);
-        UsuarioFactory::createMany(1, function (){
-                return [
-                    'username' => 'pepe',
-                    'password' => 'madrid',
-                    'conserje' => true
-                ];
-            });
+        UsuarioFactory::createOne([
+            'username' => 'pepe',
+            'password' => 'madrid',
+            'conserje' => true
+        ]);
 
         foreach ($cursosAcademicos as $cursoAcademico) {
             GrupoFactory::createMany(8, function () use ($cursoAcademico) {
@@ -61,6 +67,24 @@ class AppFixtures extends Fixture
                 ];
             });
         }
+
+        foreach ($motivos as $i => $motivo) {
+            MotivoFactory::createOne([
+                'descripcion' => $motivo,
+                'numeroOrden' => $i
+            ]);
+        }
+
+        RegistroFactory::createOne([
+            $minutos_aleatorios = rand(5, 30),
+            'horaSalida' => $hora,
+            'horaEntrada' => date('H:i:s', strtotime("+$minutos_aleatorios minutes", strtotime($hora))),
+            'responsable' => ,
+            'estudiante' => ,
+            'motivos' => ,
+            'llave'
+
+        ]);
 
         $manager->flush();
     }
