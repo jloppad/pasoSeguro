a759a794c36c9aa887c210f0b30be18f97c3b129
jloppad <jloppad@g.educaand.es>
2024-06-02 12:18:49 +0200

    Navbar terminado

--- public/js/menuHamburguesa.js
+++ public/js/menuHamburguesa.js
@@ -1,24 +1,48 @@
-function toggleLeftMenu() {
-    var menuItems = document.querySelector('.menu-items');
-    var rightMenuItems = document.querySelector('.right ul.menu-items');
-    var icon = document.querySelector('.fas .fa-bars .menu-open');
+document.addEventListener('DOMContentLoaded', main);
 
-    if (rightMenuItems.classList.contains('active')) {
-        rightMenuItems.classList.remove('active');
-    }
+function main() {
+    let left_menu = document.getElementById('left-menu-icon');
+    let right_menu = document.getElementById('right-menu-icon');
 
-    menuItems.classList.toggle('active');
+    let leftMenuItems = document.querySelector('.left .menu-items');
+    let rightMenuItems = document.querySelector('.right ul.menu-items');
 
+    left_menu.addEventListener('click', function () {
+        toggleMenu(leftMenuItems, rightMenuItems);
+        toggleIcon('left-menu-icon-i', leftMenuItems.classList.contains('active'));
+        toggleIcon('right-menu-icon-i', false);
+    });
+
+    right_menu.addEventListener('click', function () {
+        toggleMenu(rightMenuItems, leftMenuItems);
+        toggleIcon('right-menu-icon-i', rightMenuItems.classList.contains('active'));
+        toggleIcon('left-menu-icon-i', false);
+    });
 }
 
-function toggleRightMenu() {
-    var menuItems = document.querySelector('.right ul.menu-items');
-    var leftMenuItems = document.querySelector('.menu-items');
+function toggleMenu(menu, menuOppo) {
 
-    if (leftMenuItems.classList.contains('active')) {
-        leftMenuItems.classList.remove('active');
+    if (menuOppo.classList.contains('active')) {
+        menuOppo.classList.remove('active');
     }
 
-    menuItems.classList.toggle('active');
+    menu.classList.toggle('active');
 }
 
+function toggleIcon(iconId, isActive) {
+
+    let icon = document.getElementById(iconId);
+
+    if (isActive) {
+        icon.classList.remove('fa-bars', 'fa-user');
+        icon.classList.add('fa-times');
+    } else {
+        icon.classList.remove('fa-times');
+        if (iconId === 'left-menu-icon-i') {
+            icon.classList.add('fa-bars');
+        } else {
+            icon.classList.add('fa-user');
+        }
+    }
+
+}
