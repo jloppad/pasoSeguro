68e6d14a49cfcf34be27b7293b4c3f30ece580bf
jloppad <lopezpadillaj03@gmail.com>
2024-06-07 13:52:33 +0200

    Cambio de color para las cards de estudiantes

---
+++ public/js/cambiarColorCard.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', main);
+
+function main() {
+    const checkboxes = document.querySelectorAll(".card input[type='checkbox']");
+
+    checkboxes.forEach(function(checkbox) {
+        checkbox.addEventListener("change", function() {
+            const card = this.closest(".card");
+            if (card) {
+                const checkedCheckboxes = card.querySelectorAll("input[type='checkbox']:checked");
+                if (checkedCheckboxes.length > 0) {
+                    card.classList.add("active-card");
+                    card.classList.remove("inactive-card");
+                } else {
+                    card.classList.add("inactive-card");
+                    card.classList.remove("active-card");
+                }
+            }
+        });
+    });
+}
