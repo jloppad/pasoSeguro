1d114bb230b2e4dc357344849aa6d9607f1d683b
jloppad <lopezpadillaj03@gmail.com>
2024-06-11 12:47:32 +0200

    Modificacion

--- src/Controller/FotoController.php
+++ src/Controller/FotoController.php
@@ -9,6 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class FotoController extends AbstractController
 {
+    // Implementacion: src="{{ path('foto_estudiante', {'id': estudiante.id}) }}"
     #[Route('/foto/{id}', name: 'foto_estudiante')]
     public function getFotoAction(Estudiante $estudiante): StreamedResponse
     {
